4520340c9406efdc02402e3b9db4dd21f341b6f8
Felix Samsonov <german1samsonov@gmail.com>
2025-03-06 15:12:33 +0200

    HW2.3 "Struct"

---
+++ StructNumber/StructNumber/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructNumber;
+
+public struct DecimalNumber
+{
+    public int Number { get; set; }
+
+    public DecimalNumber(int number) { Number = number; }
+
+    public void Binary()
+    {
+        string binary = Convert.ToString(Number, 2);
+        Console.WriteLine($"Your number in binary system is: {binary} ");
+    }
+    public void Octal()
+    {
+        string octal = Convert.ToString(Number, 8);
+        Console.WriteLine($"Your number in octal system is: {octal} ");
+    }
+    public void Hex()
+    {
+        string hex = Convert.ToString(Number, 16).ToUpper();
+        Console.WriteLine($"Your number in hexadecimal system is: {hex}");
+    }
+
+}
---
+++ StructNumber/StructNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StructNumber;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DecimalNumber decimalNumber = new DecimalNumber(550);
+        decimalNumber.Binary();
+        decimalNumber.Octal();
+        decimalNumber.Hex();
+    }
+}
