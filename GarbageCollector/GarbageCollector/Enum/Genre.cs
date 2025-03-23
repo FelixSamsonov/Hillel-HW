b3e1b3a8ae4f355f5b4972e3c3c6478b5c4682b0
Felix Samsonov <german1samsonov@gmail.com>
2025-03-23 12:50:03 +0200

    HW6 "Garbage Collector"

---
+++ GarbageCollector/GarbageCollector/Enum/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarbageCollector.Enum
+{
+    enum Genre
+    {
+        Unknown = 1,
+        Comedy,
+        Tragedy,
+        Drama,
+        Tragicomedy,
+        Farce,
+        Vaudeville
+    }
+}
