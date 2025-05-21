0256f887b51897033aed74dc6a6f677d131b23e5
Felix Samsonov <german1samsonov@gmail.com>
2025-05-22 00:05:16 +0300

    HW-13 "ДЗ 13. ASP.NET Core MVC"

---
+++ NoteBook/NoteBook/Data/NoteBookDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoteBook.Models;
+
+namespace NoteBook.Data
+{
+    public class NoteBookDbContext : DbContext
+    {
+        public NoteBookDbContext (DbContextOptions<NoteBookDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ NoteBook/NoteBook/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteBook.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; } 
+        public DateTime DueDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
